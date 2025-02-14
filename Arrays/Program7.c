f91d8167d23da098fe06e9f91ba7dad1bcc1c405
PrajwalR09 <rathodprajwal09@gmail.com>
2025-02-14 14:29:55 +0530

    Added program to remove duplicate elements from an array

--- Arrays/Program7.c
+++ Arrays/Program7.c
@@ -1,1 +1,32 @@
-/*Yet to write a program*/
+/*Program to remove duplicate elements from an unsorted array*/
+
+#include<stdio.h>
+
+int main(void){
+
+    int arr[] = {1,2,1,2,3,2,1};
+    int i, j, k, len, pos, duplicate;
+
+    len = sizeof(arr) / sizeof(arr[0]);
+
+    for(i = 0; i < len; i++){
+        duplicate = arr[i];
+        for(j = i+1; j < len; j++){
+            if(arr[j] == duplicate){
+                for(k = j; k < len-1; k++){
+                    arr[k] = arr[k+1];
+                }
+                    len--;
+                    j--;
+            }
+
+        }
+    }
+
+    printf("The array after removing duplicates is:\n");
+
+    for(i = 0; i < len; i++)
+        printf("%d ",arr[i]);
+
+    return 0;
+}
